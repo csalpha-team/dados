83a33636239b9cfad373c4763c02c2b60da6ec9f
cleytoncandeira <cleytonacandeira@gmail.com>
2026-04-17 07:56:38 -0300

    fix(gold): remove unused percentage testing utilities

--- tests/test_coefficient_identity.py
+++ tests/test_coefficient_identity.py
@@ -7,12 +7,6 @@ import pandas as pd
 from dados.gold.br_coeficientes_consumo.testing_utils import (
     validar_cenario_coeficientes_unitarios as validar_consumo_unitario,
 )
-from dados.gold.br_coeficientes_investimento.testing_utils import (
-    validar_cenario_coeficientes_unitarios as validar_investimento_unitario,
-)
-from dados.gold.br_coeficientes_renda.testing_utils import (
-    validar_cenario_coeficientes_unitarios as validar_renda_unitaria,
-)
 from dados.gold.pa_coeficientes_custo.testing_utils import (
     validar_cenario_coeficientes_unitarios as validar_custo_unitario,
 )
@@ -45,26 +39,6 @@ class CoefficientIdentityTests(unittest.TestCase):
     def test_cost_coefficients_can_be_forced_to_one(self) -> None:
         coefficients, expected_keys = validar_custo_unitario()
         self.assert_all_coefficients_are_one(coefficients, "tipo_coeff", expected_keys)
-
-    def test_investment_coefficients_can_be_forced_to_one(self) -> None:
-        coefficients, expected_keys = validar_investimento_unitario()
-        self.assert_all_coefficients_are_one(coefficients, "coeff_key", expected_keys)
-
-    def test_income_coefficients_can_be_forced_to_one(self) -> None:
-        coefficients, productivity_output, salary_output, expected_accounts = validar_renda_unitaria()
-
-        self.assert_all_coefficients_are_one(
-            coefficients.loc[coefficients["tipo_coeff"] == "prod_mon_trab"],
-            "conta_alfa",
-            expected_accounts,
-        )
-        self.assert_all_coefficients_are_one(
-            coefficients.loc[coefficients["tipo_coeff"] == "salario_medio"],
-            "conta_alfa",
-            expected_accounts,
-        )
-        self.assert_all_coefficients_are_one(productivity_output, "conta_alfa", expected_accounts)
-        self.assert_all_coefficients_are_one(salary_output, "conta_alfa", expected_accounts)
 
 
 if __name__ == "__main__":
